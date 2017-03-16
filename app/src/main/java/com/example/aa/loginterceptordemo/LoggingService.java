3a38676d6a702e611aa6c22849afe71c9b0043b1
aa <bit@treebo123>
2017-03-16 16:45:38 +0530

    code refactoring

--- app/src/main/java/com/example/aa/loginterceptordemo/LoggingService.java
+++ app/src/main/java/com/example/aa/loginterceptordemo/LoggingService.java
@@ -2,13 +2,9 @@ package com.example.aa.loginterceptordemo;
 
 import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
+import android.os.Process;
 import android.support.annotation.Nullable;
-import android.support.annotation.WorkerThread;
 import android.util.Log;
 
 import java.io.BufferedReader;
@@ -16,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 
-import io.socket.client.Ack;
 import io.socket.client.IO;
 import io.socket.client.Socket;
 
@@ -26,166 +21,67 @@ import io.socket.client.Socket;
 
 public class LoggingService extends Service {
 
-    private volatile Looper mServiceLooper;
-    private volatile ServiceHandler mServiceHandler;
-    private String mName;
-    private boolean mRedelivery;
-    private boolean mIsSocketConnected = false;
-
-    private static final String processId = Integer.toString(android.os.Process.myPid());
-    public static final String TAG = LoggingService.class.getSimpleName();
     private Socket mSocket;
-
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
-            super(looper);
-        }
-
-        @Override
-        public void handleMessage(Message msg) {
-            onHandleIntent((Intent) msg.obj);
-            //don't stop this service. it will get killed with its app's process
-            //stopSelf(msg.arg1);
-        }
-    }
-
-    public LoggingService() {
-        this(TAG);
-    }
-
-    /**
-     * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     * @param name Used to name the worker thread, important only for debugging.
-     */
-    private LoggingService(String name) {
-        super();
-        mName = name;
-    }
-
-    /**
-     * Sets intent redelivery preferences.  Usually called from the constructor
-     * with your preferred semantics.
-     * <p>
-     * <p>If enabled is true,
-     * {@link #onStartCommand(Intent, int, int)} will return
-     * {@link Service#START_REDELIVER_INTENT}, so if this process dies before
-     * {@link #onHandleIntent(Intent)} returns, the process will be restarted
-     * and the intent redelivered.  If multiple Intents have been sent, only
-     * the most recent one is guaranteed to be redelivered.
-     * <p>
-     * <p>If enabled is false (the default),
-     * {@link #onStartCommand(Intent, int, int)} will return
-     * {@link Service#START_NOT_STICKY}, and if the process dies, the Intent
-     * dies along with it.
-     */
-    public void setIntentRedelivery(boolean enabled) {
-        mRedelivery = enabled;
-    }
+    private static final String processId = Integer.toString(android.os.Process.myPid());
+    private static final String TAG = LoggingService.class.getSimpleName();
 
     @Override
     public void onCreate() {
-        // TODO: It would be nice to have an option to hold a partial wakelock
-        // during processing, and to have a static startService(Context, Intent)
-        // method that would launch the service & hand off a wakelock.
-
         super.onCreate();
-        HandlerThread thread = new HandlerThread("IntentService[" + mName + "]");
-        thread.start();
-
-        mServiceLooper = thread.getLooper();
-        mServiceHandler = new ServiceHandler(mServiceLooper);
+        new LoggingThread().start();
     }
 
-    /**
-     * You should not override this method for your IntentService. Instead,
-     * override {@link #onHandleIntent}, which the system calls when the IntentService
-     * receives a start request.
-     *
-     * @see android.app.Service#onStartCommand
-     */
     @Override
     public int onStartCommand(@Nullable Intent intent, int flags, int startId) {
-        mServiceHandler.sendEmptyMessage(0);
-        return mRedelivery ? START_REDELIVER_INTENT : START_NOT_STICKY;
+        return START_NOT_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        mServiceLooper.quit();
+        if (mSocket != null && mSocket.connected()) {
+            mSocket.disconnect();
+            mSocket.close();
+            mSocket = null;
+        }
+        super.onDestroy();
     }
 
-    /**
-     * Unless you provide binding for your service, you don't need to implement this
-     * method, because the default implementation returns null.
-     *
-     * @see android.app.Service#onBind
-     */
     @Override
     @Nullable
     public IBinder onBind(Intent intent) {
         return null;
     }
 
-    /**
-     * This method is invoked on the worker thread with a request to process.
-     * Only one Intent is processed at a time, but the processing happens on a
-     * worker thread that runs independently from other application logic.
-     * So, if this code takes a long time, it will hold up other requests to
-     * the same IntentService, but it will not hold up anything else.
-     * When all requests have been handled, the IntentService stops itself,
-     * so you should not call {@link #stopSelf}.
-     *
-     * @param intent The value passed to {@link
-     *               android.content.Context#startService(Intent)}.
-     *               This may be null if the service is being restarted after
-     *               its process has gone away; see
-     *               {@link android.app.Service#onStartCommand}
-     *               for details.
-     */
-    @WorkerThread
-    private void onHandleIntent(@Nullable Intent intent) {
-        try {
-            mSocket = IO.socket("http://10.0.2.2:3000");
-            mSocket.connect();
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-            stopSelf();
-            return;
+    public class LoggingThread extends Thread {
+        public LoggingThread() {
+            super("[" + TAG + "]");
         }
 
-        if(mSocket.connected()) {
-            Log.d(TAG, "MARA LO");
-        }
-
-        try {
-            String[] command = new String[]{"logcat", "-v", "threadtime"};
-
-            Process process = Runtime.getRuntime().exec(command);
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line;
-
-            while ((line = bufferedReader.readLine()) != null) {
-                if (line.contains(processId)) {
-                    //TODO send to mSocket
-                    mSocket.emit("new message", line);
-                }
+        @Override
+        public void run() {
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+            try {
+                mSocket = IO.socket("http://10.0.2.2:3000");
+                mSocket.connect();
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+                stopSelf();
+                return;
             }
-        } catch (IOException ex) {
-            Log.e(TAG, "start failed", ex);
-        }
-    }
 
-    private void connect() {
-        try {
-            mSocket = IO.socket("http://10.0.2.2:3000");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        if(mSocket != null) {
-            mSocket.connect();
+            try {
+                String[] command = new String[]{"logcat", "-v", "threadtime"};
+                java.lang.Process process = Runtime.getRuntime().exec(command);
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    if (line.contains(processId)) {
+                        mSocket.emit("new message", line);
+                    }
+                }
+            } catch (IOException ex) {
+                Log.e(TAG, "start failed", ex);
+            }
         }
     }
 }
